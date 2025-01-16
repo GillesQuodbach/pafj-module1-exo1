8d1bcd17e77188ee67f2c6725ad22f1723354fa3
Gilles <quodbach.gilles@gmail.com>
2025-01-16 13:47:35 +0100

    fix

--- test_nombres.py
+++ test_nombres.py
@@ -1,12 +1,16 @@
 import sys
 
+
+# https://github.com/GillesQuodbach/pafj-module1-exo1.git
+
 def negative_or_positive(input_number):
     if input_number > 0:
         return "positif"
     elif input_number < 0:
-        return "negatif"
+        return "négatif"
     else:
-        return "null"
+        return "nul"
+
 
 def odd_or_even(input_number):
     if input_number % 2 == 0:
@@ -14,14 +18,14 @@ def odd_or_even(input_number):
     else:
         return "impair"
 
-if len(sys.argv) > 1:
-# Question 3
-    try:
-# Question 1
-        numbers = list(map(int, sys.argv[1:]))
 
+if len(sys.argv) > 1:
+    # Question 3
+    try:
+        # Question 1
+        numbers = list(map(int, sys.argv[1:]))
+        # Question 2
         for number in numbers:
-# Question 2
             print(f"{number}: {negative_or_positive(number)} et {odd_or_even(number)}")
     except ValueError:
         print("Votre liste n'est pas valide, erreur de saisie !!!")
